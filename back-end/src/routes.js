bc91a2add99da35bd7ebdc4c089952d603d1ed2b
LuisCL94 <lclaudio.tl@gmail.com>
2020-06-27 02:32:18 -0300

    List user appointments and provider schedule

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 import logRequest from './app/middlewares/logRequest';
@@ -26,6 +27,9 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.list);
 
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.list);
+
+routes.get('/schedule', ScheduleController.list);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
