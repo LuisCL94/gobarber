fbc257ed130593ae024be6e609a26d53456f3cc2
LuisCL94 <lclaudio.tl@gmail.com>
2020-07-03 19:19:03 -0300

    Notify providers with email templates when an appointment is canceled

--- back-end/src/app/controllers/AppointmentController.js
+++ back-end/src/app/controllers/AppointmentController.js
@@ -5,6 +5,7 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
   async list(req, res) {
@@ -109,6 +110,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        }
       ],
     });
 
@@ -129,7 +135,20 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
-
+    
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
+    });
+    
     return res.json(appointment);
   }
 }
---
+++ back-end/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'f7f3ec1da98275',
+    pass: 'acd58d34a79e9c',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
