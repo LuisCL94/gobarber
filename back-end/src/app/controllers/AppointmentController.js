182bea6189c2f10b3593943c96f3ebb3f3f565a3
LuisCL94 <lclaudio.tl@gmail.com>
2020-07-06 18:59:09 -0300

    Add virtua fields that inform if appointments have already passed and are cancelable

--- back-end/src/app/controllers/AppointmentController.js
+++ back-end/src/app/controllers/AppointmentController.js
@@ -14,7 +14,7 @@ class AppointmentController {
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       include: [
         {
           model: User,
