290f9adc90b99991f51f88b0cb1a00b2a9bffb13
LuisCL94 <lclaudio.tl@gmail.com>
2020-07-19 04:43:47 -0300

    Configure header, update user and logout the application

--- front-end-web/src/pages/Dashboard/index.js
+++ front-end-web/src/pages/Dashboard/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import api from '~/services/api';
+
 // import { } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
   return (
     <>
       <h1>Hello Dashboard World!</h1>
--- front-end-web/src/pages/Profile/index.js
+++ front-end-web/src/pages/Profile/index.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import AvatarInput from './AvatarInput';
+
+import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector((state) => state.user.profile);
+  const dispatch = useDispatch();
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
   return (
-    <>
-      <h1>Hello Profile World!</h1>
-    </>
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" placeholder="Seu endereço completo" />
+        <hr />
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="submit" onClick={handleSignOut}>
+        Sair do Gobarber
+      </button>
+    </Container>
   );
 }
