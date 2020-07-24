0aae960da30dd24ec351b2e440cbb1bed6273368
LuisCL94 <lclaudio.tl@gmail.com>
2020-07-24 01:48:12 -0300

    Start mobile project

---
+++ mobile/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, StyleSheet, StatusBar} from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor='#7519c1'/>
+      <View style={styles.container}>
+        <Text style={styles.title} > Hello Wjkhkorld </Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7519c1',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  title: {
+    color: '#FFF',
+    fontSize: 32,
+    fontWeight: "bold",
+  }
+})
