319ba159e8e0e32e2bfe40c2d67484c8e41d0cfb
LuisCL94 <lclaudio.tl@gmail.com>
2020-07-24 02:30:08 -0300

    Configure project style guide and root import

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet, StatusBar} from 'react-native';
-
-export default function App() {
-  return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor='#7519c1'/>
-      <View style={styles.container}>
-        <Text style={styles.title} > Hello Wjkhkorld </Text>
-      </View>
-    </>
-  );
-}
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7519c1',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
 
   title: {
     color: '#FFF',
     fontSize: 32,
-    fontWeight: "bold",
-  }
-})
+    fontWeight: 'bold',
+  },
+});
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7519c1" />
+      <View style={styles.container}>
+        <Text style={styles.title}> Hello World </Text>
+      </View>
+    </>
+  );
+}
